03e2c057e6736a12456220333275d71deede2d17
fstufop <fstufop@gmail.com>
2021-09-19 10:46:01 -0300

    chore: init project

---
+++ Desafio_UseAcademy_1/Desafio_UseAcademy_1/Sources/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Desafio_UseAcademy_1
+//
+//  Created by BootCamp on 19/09/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
