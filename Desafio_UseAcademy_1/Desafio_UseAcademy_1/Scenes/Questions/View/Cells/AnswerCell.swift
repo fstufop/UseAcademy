35e96761aead03a0639d13db5073ee31b87aa5d4
fstufop <fstufop@gmail.com>
2021-09-20 16:55:38 -0300

    fix: darkmode adjustment

--- Desafio_UseAcademy_1/Desafio_UseAcademy_1/Scenes/Questions/View/Cells/AnswerCell.swift
+++ Desafio_UseAcademy_1/Desafio_UseAcademy_1/Scenes/Questions/View/Cells/AnswerCell.swift
@@ -9,12 +9,12 @@ import UIKit
 
 class AnswerCell: UITableViewCell {
     //MARK: - Properties
-    
+
     //MARK: - Outlets
 
     @IBOutlet weak var alternativeLabel: UILabel!
     @IBOutlet weak var alternativeViewBackgound: UIView!
-    
+    @IBOutlet weak var checkbox: UIImageView!
     
     //MARK: - Actions
     
@@ -26,34 +26,57 @@ class AnswerCell: UITableViewCell {
     
     //MARK: - Methods
     func setup(model: AnswerModel){
+        
         alternativeLabel.text = model.name
         
         alternativeViewBackgound.layer.cornerRadius = 15
         alternativeViewBackgound.layer.borderWidth = 1
-        
+
         setStyle(model.style)
     }
     func setStyle(_ style: AnswerModel.Style) {
         switch style {
-            case .blank:
-                alternativeViewBackgound.backgroundColor = .white
-                alternativeViewBackgound.layer.borderColor = UIColor.gray.cgColor
-                alternativeLabel.textColor = .black
-            case .selected:
-                alternativeViewBackgound.backgroundColor = UIColor.init(red: 117/255, green: 140/255, blue: 255/255, alpha: 0.2)
-                alternativeViewBackgound.layer.borderColor = UIColor.blue.cgColor
-                alternativeLabel.textColor = UIColor.init(red: 96/255, green: 97/255, blue: 255/255, alpha: 1)
-          
-            case .correct:
-                alternativeViewBackgound.backgroundColor = UIColor.init(red: 145/255, green: 226/255, blue: 149/255, alpha: 0.1)
-                alternativeViewBackgound.layer.borderColor = UIColor.green.cgColor
-                alternativeLabel.textColor = UIColor.init(red: 56/255, green: 197/255, blue: 61/255, alpha: 1)
-           
-            case .incorrect:
-                alternativeViewBackgound.backgroundColor = UIColor.init(red: 251/255, green: 101/255, blue: 174/255, alpha: 0.2)
-                alternativeViewBackgound.layer.borderColor = UIColor.red.cgColor
-                alternativeLabel.textColor = UIColor.init(red: 245/255, green: 101/255, blue: 103/255, alpha: 1)
+        case .blank:
+            alternativeViewBackgound.backgroundColor = .white
+            alternativeViewBackgound.layer.borderColor = UIColor.gray.cgColor
+            alternativeLabel.textColor = .black
+            checkbox.tintColor = .blue
+            if #available(iOS 13.0, *) {
+                checkbox.image = UIImage(systemName: "square")
+            } else {
+                // Fallback on earlier versions
+            }
+        case .selected:
+            alternativeViewBackgound.backgroundColor = UIColor.init(red: 117/255, green: 140/255, blue: 255/255, alpha: 0.2)
+            alternativeViewBackgound.layer.borderColor = UIColor.blue.cgColor
+            alternativeLabel.textColor = UIColor.init(red: 96/255, green: 97/255, blue: 255/255, alpha: 1)
+            checkbox.tintColor = .blue
+            if #available(iOS 13.0, *) {
+                checkbox.image = UIImage(systemName: "checkmark.square.fill")
+            } else {
+                // Fallback on earlier versions
+            }
+        case .correct:
+            alternativeViewBackgound.backgroundColor = UIColor.init(red: 145/255, green: 226/255, blue: 149/255, alpha: 0.1)
+            alternativeViewBackgound.layer.borderColor = UIColor.green.cgColor
+            alternativeLabel.textColor = UIColor.init(red: 56/255, green: 197/255, blue: 61/255, alpha: 1)
+            checkbox.tintColor = .green
+            if #available(iOS 13.0, *) {
+                checkbox.image = UIImage(systemName: "checkmark.square.fill")
+            } else {
+                // Fallback on earlier versionscheckmark.square.fill
+            }
+        case .incorrect:
+            alternativeViewBackgound.backgroundColor = UIColor.init(red: 251/255, green: 101/255, blue: 174/255, alpha: 0.2)
+            alternativeViewBackgound.layer.borderColor = UIColor.red.cgColor
+            alternativeLabel.textColor = UIColor.init(red: 245/255, green: 101/255, blue: 103/255, alpha: 1)
+            checkbox.tintColor = .red
+            if #available(iOS 13.0, *) {
+                checkbox.image = UIImage(systemName: "square.split.diagonal.2x2.fill")
+            } else {
+                // Fallback on earlier versionscheckmark.square.fill
+            }
+            
         }
-        
     }
 }
